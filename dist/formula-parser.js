f662cd0d94f6753a376bd5cfedbfef29e9b4791a
Ryan Scott <ryanscott161@gmail.com>
2017-10-24 18:06:03 +1300

    Update distilled

--- dist/formula-parser.js
+++ dist/formula-parser.js
@@ -635,8 +635,18 @@ return /******/ (function(modules) { // webpackBootstrap
 	
 	function func(first) {
 	  try {
+	    if (first === undefined || first === '') {
+	      first = 0;
+	    }
+	
 	    for (var _len = arguments.length, rest = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
 	      rest[_key - 1] = arguments[_key];
+	    }
+	
+	    for (var i = 0; i < rest.length; i++) {
+	      if (rest[i] === undefined || rest[i] === '') {
+	        rest[i] = 0;
+	      }
 	    }
 	
 	    var result = rest.reduce(function (acc, value) {
